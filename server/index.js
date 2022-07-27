3dd65bf5ab3943eedbfc91f150e13692c7af49c0
Sudip Shrestha <xthazudp@gmail.com>
2022-07-27 17:49:26 +0545

    basic MVC setup

--- server/index.js
+++ server/index.js
@@ -3,11 +3,15 @@ import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
 
+import postRoutes from './routes/posts.js';
+
 const app = express();
 
 app.use(bodyParser.json({ limit: '30mb', extended: true }));
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
 app.use(cors());
+
+app.use('/posts', postRoutes);
 
 const CONNECTION_URL =
   'mongodb+srv://xthazudp:memoryproject123@cluster0.90pix.mongodb.net/?retryWrites=true&w=majority';
