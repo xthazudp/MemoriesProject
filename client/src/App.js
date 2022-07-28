32c0c559b8c2cc8e1db9c3f280f686481df3468c
Sudip Shrestha <xthazudp@gmail.com>
2022-07-28 08:41:58 +0545

    redux implement

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,8 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
+import { useDispatch } from 'react-redux';
+
+import { getPosts } from './actions/posts.js';
 
 import memories from './images/memories.png';
 import Posts from './components/Posts/Posts.js';
@@ -8,6 +11,12 @@ import useStyles from './styles.js';
 
 const App = () => {
   const classes = useStyles();
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getPosts());
+  }, [dispatch]);
+
   return (
     <Container maxidth='lg'>
       <AppBar className={classes.appBar} position='static' color='inherit'>
